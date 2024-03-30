7e251dbf51b7a8829f41a97e84dcd45acd565d26
NateDavid324 <81783600+Nate231255Khoo@users.noreply.github.com>
2024-03-29 19:47:31 -0500

    Update PyOS.py

--- PyOS.py
+++ PyOS.py
@@ -1,7 +1,42 @@
 import OS, pytz, datetime
 
+print(
+'''
+|============================PyOS===============================|
+|                       /==============|                        |
+|   |\  | | /          / 0             |                        |
+|   | \ | |<          |                |                        |
+|   |  \| | \         |                |                        |
+|              /======|========|       |======\                 |
+|              |                       |      |                 |
+|              |       Python : 3.12.2 |      |                 |
+|              |                       |      |                 |
+|              |      |----------------|      |                 |
+|              |      |                       |                 |
+|              |      | PyOS  : 0.1.4         |                 |
+|              |      |                       |                 |
+|              \======|        |=======|======/                 |
+|                     |                |                        |
+|                     |                |                        |
+|                     |              0 /                        |
+|                     |===============/                         |
+|===============================================================|
+'''
+)
 while True:
-    j = input(f"P:/users/{OS.user.user}/").split()
-    local_now = datetime.datetime.now(pytz.timezone(OS.user.users[OS.user.user]["tz"]))
-    OS.OS.log += f"\n[{local_now.strftime('%H:%M:%S')}] {''.join(j)}"
-    OS.OS.parse(j)
+    try:
+        OS.user.users = OS.cache_user_data.load()
+        j = input(f"P:/users/{OS.user.user}/").split()
+        local_now = datetime.datetime.now(pytz.timezone(OS.user.users[OS.user.user]["tz"]))
+        OS.OS.log += f"\n[{local_now.strftime('%H:%M:%S')}] {''.join(j)}"
+        OS.OS.parse(j)
+    except Exception as err:
+        f = open('log.txt','r+')
+        f.write(f"{str(OS.OS.log).lstrip()}\n{err} occured")
+        f.close()
+    except KeyboardInterrupt:
+        f = open('log.txt','r+')
+        f.write(f"{str(OS.OS.log).lstrip()}")
+        f.close()
+        
+        break
